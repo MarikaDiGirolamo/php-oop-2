b00490f147de93addc500e501b663b13457fa5d4
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-06-21 15:10:12 +0200

    create index and category,php

---
+++ products.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/models/category.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-2</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
